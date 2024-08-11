c345b34fb6e4fb9ebc760e74e458248e71fb6005
Karan <kr123561@gmail.com>
2024-08-11 16:48:30 +0530

    Update lambdaFunction.py

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -18,4 +18,23 @@ def lambda_handler(event, context):
         Size=100 
     )   
     
-    
+
+
+#event = {
+#   "version":"0",
+#   "id":"5a06b07c-7d8b-1fb1-85c8-ca0a581397b9",
+#   "detail-type":"EBS Volume Notification",
+#   "source":"aws.ec2",
+#   "account":"021891585312",
+#   "time":"2024-08-11T11:11:51Z",
+#   "region":"eu-north-1",
+#   "resources":[
+#       "arn:aws:ec2:eu-north-1:021891585312:volume/vol-0f0e09dd2ec13c47a"
+#    ],
+#    "detail":{
+#       "result":"available",
+#       "cause":"",
+#       "event":"createVolume",
+#       "request-id":"6ec0b4ea-d6c9-450f-950f-a51b746dbd80"
+#    }
+# }
